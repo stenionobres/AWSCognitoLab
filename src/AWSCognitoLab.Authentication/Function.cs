9e6c3574979e1d7677bb75d4f03d4f797e7ffce5
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-09-28 19:02:16 -0300

    Clean authentication function code

--- src/AWSCognitoLab.Authentication/Function.cs
+++ src/AWSCognitoLab.Authentication/Function.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -12,13 +7,6 @@ namespace AWSCognitoLab.Authentication
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
         public string FunctionHandler(string input, ILambdaContext context)
         {
             return input?.ToUpper();
