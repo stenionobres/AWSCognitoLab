0a4c60aa21f3d6af29a23521decc77724bc185c6
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-11-09 20:13:44 -0300

    Adds SignUp project to solution

---
+++ src/AWSCognitoLab.SignUp/SignUpFunction.cs
@@ -0,0 +1,15 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSCognitoLab.SignUp
+{
+    public class SignUpFunction
+    {
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input?.ToUpper();
+        }
+    }
+}
